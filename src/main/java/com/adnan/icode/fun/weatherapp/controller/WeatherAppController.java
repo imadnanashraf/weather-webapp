feac896e708695dfdd61dcb74c342707a1e7c1b9
imadnanashraf <adnan.ashraf19@gmail.com>
2022-02-04 21:34:11 +0530

    version 7 Exception handled

--- src/main/java/com/adnan/icode/fun/weatherapp/controller/WeatherAppController.java
+++ src/main/java/com/adnan/icode/fun/weatherapp/controller/WeatherAppController.java
@@ -98,159 +98,163 @@ public class WeatherAppController {
 		
 		
 		//get current city data
+		try {
+			CurrentWeatherByCity currentWeather = currentWeatherService.
+					  getCurrentWeatherByCity(theCityName,mode);
+			
+			Weather cWeather = currentWeather.getWeather().get(0);
+			Temperature cTemp	= currentWeather.getMain();
+			Wind cWind = currentWeather.getWind();
+			SysInfo cSys = currentWeather.getSys();
+			//testing
+			CoOrdinates cCor = currentWeather.getCoord();
+			
+			//get current city pollution data
+			CurrentAirPollution currentAirPollutionTemplate = currentWeatherService.
+													  getAirPollutionData(cCor.getLat(), cCor.getLon());
+			
+			AirPollutionIndex currentAirPollution =	currentAirPollutionTemplate.getList().get(0);
+			
+			//adding airpollution to model attribute
+			
+			float airIndex = currentAirPollution.getMain().getAqi();
+			theModel.addAttribute("airIndex", airIndex);
+			
+			AirTester tempAirTester = new AirTester();
+			String AirHealth = tempAirTester.airIndexTester(airIndex);
+			theModel.addAttribute("review", AirHealth);
+			
+			airIndex = airIndex / 5 * 100;
+			int airIndexPercent = (int) airIndex;
+			theModel.addAttribute("airIndexPercent", airIndexPercent);
+			
+			float no2 = currentAirPollution.getComponents().getNo2();
+			theModel.addAttribute("no2", no2);
+			
+			no2 = no2 / 400 * 100;
+			int no2Percent = (int) no2;
+			
+			// so that percent does not exceed 100
+			if(no2Percent >= 100) {
+				no2Percent = 100;
+			}
+			
+			theModel.addAttribute("no2Percent", no2Percent);
+			System.out.println(no2);
+			
+			float pm10 = currentAirPollution.getComponents().getPm10();
+			theModel.addAttribute("pm10", pm10);
+			
+			pm10 = pm10/180 * 100;
+			int pm10Percent = (int) pm10;
+			
+			// so that percent does not exceed 100
+			if(pm10Percent >= 100) {
+				pm10Percent = 100;
+			}
+			
+			theModel.addAttribute("pm10Percent", pm10Percent);
+			
+			float o3 = currentAirPollution.getComponents().getO3();
+			theModel.addAttribute("o3", o3);
+			
+			o3 = o3 / 240 * 100;
+			int o3Percent = (int) o3;
+			
+			// so that percent does not exceed 100
+			if(o3Percent >= 100) {
+				o3Percent = 100;
+			}
+			
+			theModel.addAttribute("o3Percent", o3Percent);
+			
+			float pm25 = currentAirPollution.getComponents().getPm2_5();
+			theModel.addAttribute("pm25", pm25);
+			
+			pm25 = pm25 / 110 * 100;
+			int pm25Percent = (int) pm25;
+			
+			// so that percent does not exceed 100
+			if(pm25Percent >= 100) {
+				pm25Percent = 100;
+			}
+			
+			theModel.addAttribute("pm25Percent", pm25Percent);
+			
+			
+			//weather
+			theModel.addAttribute("main", cWeather.getMain());
+			theModel.addAttribute("description", cWeather.getDescription());
+			theModel.addAttribute("icon", cWeather.getIcon());
+			
+			//temp
+			theModel.addAttribute("temp", cTemp.getTemp());
+			theModel.addAttribute("temp_min", cTemp.getTemp_min());
+			theModel.addAttribute("temp_max", cTemp.getTemp_max());
+			theModel.addAttribute("pressure", cTemp.getPressure());
+			theModel.addAttribute("pressure", cTemp.getPressure());
+			
+			//visiblity
+			theModel.addAttribute("visibility", currentWeather.getVisibility());
+			
+			//wind
+			theModel.addAttribute("speed", cWind.getSpeed());
+			theModel.addAttribute("gust", cWind.getGust());
+			
+			//SysInfo
+			theModel.addAttribute("country", cSys.getCountry());
+			
+			HumanTime temHumanTime = new HumanTime();
+			String sunrise = temHumanTime.unixTimeToHumanReadable(cSys.sunrise+currentWeather.getTimezone());
+			String sunset = temHumanTime.unixTimeToHumanReadable(cSys.sunset+currentWeather.getTimezone());
+			
+			theModel.addAttribute("sunrise", sunrise);
+			theModel.addAttribute("sunset", sunset);
+			
+			theModel.addAttribute("lat", cCor.getLat());
+			theModel.addAttribute("lng", cCor.getLon());
 		
-		CurrentWeatherByCity currentWeather = currentWeatherService.
-											  getCurrentWeatherByCity(theCityName,mode);
-				
+			
+			// name
+			theModel.addAttribute("name", currentWeather.getName());
+			System.out.println(currentWeather.getName());
+			
+			theCityName = currentWeather.getName().replaceAll(" ", "-");
 		
-		
-		Weather cWeather = currentWeather.getWeather().get(0);
-		Temperature cTemp	= currentWeather.getMain();
-		Wind cWind = currentWeather.getWind();
-		SysInfo cSys = currentWeather.getSys();
-		//testing
-		CoOrdinates cCor = currentWeather.getCoord();
-		
-		//get current city pollution data
-		CurrentAirPollution currentAirPollutionTemplate = currentWeatherService.
-												  getAirPollutionData(cCor.getLat(), cCor.getLon());
-		
-		AirPollutionIndex currentAirPollution =	currentAirPollutionTemplate.getList().get(0);
-		
-		//adding airpollution to model attribute
-		
-		float airIndex = currentAirPollution.getMain().getAqi();
-		theModel.addAttribute("airIndex", airIndex);
-		
-		AirTester tempAirTester = new AirTester();
-		String AirHealth = tempAirTester.airIndexTester(airIndex);
-		theModel.addAttribute("review", AirHealth);
-		
-		airIndex = airIndex / 5 * 100;
-		int airIndexPercent = (int) airIndex;
-		theModel.addAttribute("airIndexPercent", airIndexPercent);
-		
-		float no2 = currentAirPollution.getComponents().getNo2();
-		theModel.addAttribute("no2", no2);
-		
-		no2 = no2 / 400 * 100;
-		int no2Percent = (int) no2;
-		
-		// so that percent does not exceed 100
-		if(no2Percent >= 100) {
-			no2Percent = 100;
+			//creating cookie
+			Cookie weatherCookie = new Cookie("weatherApp.city", theCityName);
+			
+			//cookie life span
+			weatherCookie.setMaxAge(60*60*24*365);
+			
+			//adding the cookie
+			response.addCookie(weatherCookie);
+			
+			//creating cookie for lat and lng
+			
+			String coLatLng = cCor.getLat()+"-"+cCor.getLon();
+			
+			
+			
+			Cookie weatherLatLng = new Cookie("weatherApp.latlng",coLatLng);
+					
+			//cookie life span
+			weatherLatLng.setMaxAge(60*60*24*365);
+					
+			//adding the cookie
+			response.addCookie(weatherLatLng);
+			
+			
+			//adding maps api key
+			theModel.addAttribute("mapApiKey", mapApiKey);
+			
+			
+			
+		}catch(Exception exc) {
+			
+			return "notfound";
 		}
-		
-		theModel.addAttribute("no2Percent", no2Percent);
-		System.out.println(no2);
-		
-		float pm10 = currentAirPollution.getComponents().getPm10();
-		theModel.addAttribute("pm10", pm10);
-		
-		pm10 = pm10/180 * 100;
-		int pm10Percent = (int) pm10;
-		
-		// so that percent does not exceed 100
-		if(pm10Percent >= 100) {
-			pm10Percent = 100;
-		}
-		
-		theModel.addAttribute("pm10Percent", pm10Percent);
-		
-		float o3 = currentAirPollution.getComponents().getO3();
-		theModel.addAttribute("o3", o3);
-		
-		o3 = o3 / 240 * 100;
-		int o3Percent = (int) o3;
-		
-		// so that percent does not exceed 100
-		if(o3Percent >= 100) {
-			o3Percent = 100;
-		}
-		
-		theModel.addAttribute("o3Percent", o3Percent);
-		
-		float pm25 = currentAirPollution.getComponents().getPm2_5();
-		theModel.addAttribute("pm25", pm25);
-		
-		pm25 = pm25 / 110 * 100;
-		int pm25Percent = (int) pm25;
-		
-		// so that percent does not exceed 100
-		if(pm25Percent >= 100) {
-			pm25Percent = 100;
-		}
-		
-		theModel.addAttribute("pm25Percent", pm25Percent);
-		
-		
-		//weather
-		theModel.addAttribute("main", cWeather.getMain());
-		theModel.addAttribute("description", cWeather.getDescription());
-		theModel.addAttribute("icon", cWeather.getIcon());
-		
-		//temp
-		theModel.addAttribute("temp", cTemp.getTemp());
-		theModel.addAttribute("temp_min", cTemp.getTemp_min());
-		theModel.addAttribute("temp_max", cTemp.getTemp_max());
-		theModel.addAttribute("pressure", cTemp.getPressure());
-		theModel.addAttribute("pressure", cTemp.getPressure());
-		
-		//visiblity
-		theModel.addAttribute("visibility", currentWeather.getVisibility());
-		
-		//wind
-		theModel.addAttribute("speed", cWind.getSpeed());
-		theModel.addAttribute("gust", cWind.getGust());
-		
-		//SysInfo
-		theModel.addAttribute("country", cSys.getCountry());
-		
-		HumanTime temHumanTime = new HumanTime();
-		String sunrise = temHumanTime.unixTimeToHumanReadable(cSys.sunrise+currentWeather.getTimezone());
-		String sunset = temHumanTime.unixTimeToHumanReadable(cSys.sunset+currentWeather.getTimezone());
-		
-		theModel.addAttribute("sunrise", sunrise);
-		theModel.addAttribute("sunset", sunset);
-		
-		theModel.addAttribute("lat", cCor.getLat());
-		theModel.addAttribute("lng", cCor.getLon());
-	
-		
-		// name
-		theModel.addAttribute("name", currentWeather.getName());
-		System.out.println(currentWeather.getName());
-		
-		theCityName = currentWeather.getName().replaceAll(" ", "-");
-	
-		//creating cookie
-		Cookie weatherCookie = new Cookie("weatherApp.city", theCityName);
-		
-		//cookie life span
-		weatherCookie.setMaxAge(60*60*24*365);
-		
-		//adding the cookie
-		response.addCookie(weatherCookie);
-		
-		//creating cookie for lat and lng
-		
-		String coLatLng = cCor.getLat()+"-"+cCor.getLon();
-		
-		
-		
-		Cookie weatherLatLng = new Cookie("weatherApp.latlng",coLatLng);
-				
-		//cookie life span
-		weatherLatLng.setMaxAge(60*60*24*365);
-				
-		//adding the cookie
-		response.addCookie(weatherLatLng);
-		
-		
-		//adding maps api key
-		theModel.addAttribute("mapApiKey", mapApiKey);
-		
-		
+					
 		
 		return "currentweather";
 	}
